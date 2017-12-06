b7211368a40fc12d2cf282ffd73838d9834390d3
lovepk <2275701932@qq.com>
2017-12-07 01:08:14 +0800

    布局完善

--- src/public.js
+++ src/public.js
@@ -1,12 +1,22 @@
 $(function() {
 	var $navAside = $('.aside')
+	var $navitem = $('.aside-left li');
+	$navitem.on('click', function() {
+		$(this).addClass('active').siblings().removeClass('active');
+	})
+	$navitem.on('hover', function() {
+		$(this).addClass('active').siblings().removeClass('active');
+	})
+	$('.aside-left').on('mouseleave', function() {
+		$('.selected').addClass('active').siblings().removeClass('active');
+	})
 	$(window).scroll(function() {
 		setScrollPosi();
 	})
 	function setScrollPosi() {
 		var scrollTop = $(document).scrollTop();
 		console.log(scrollTop)
-		if(scrollTop > 100) {
+		if(scrollTop > 145) {
 			$navAside.addClass('adsorb');
 		} else {
 			$navAside.removeClass('adsorb');
